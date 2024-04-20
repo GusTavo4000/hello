a3fb9bfe4ab1501437fc2e8dade739fce0151ad5
Gustavo Bellini <161270960+GhostRacoon@users.noreply.github.com>
2024-04-20 15:16:53 -0300

    first commit

---
+++ lista-map.dart
@@ -0,0 +1,9 @@
+void main(){
+  Map usuario ={
+    'nome': 'gustavo',
+    'idade' : 18,
+    'altura' : 1.68,
+    'Gustavo.dev' : true,
+  };
+  print(usuario);
+}
---
+++ listas.dart
@@ -0,0 +1,6 @@
+void main(){
+  List listaProdutos = [
+  1, 2, 3, 4, 5, 'macarrao', true   
+  ];
+  print (listaProdutos);
+}
